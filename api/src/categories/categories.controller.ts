da010ddbe79bc645d136fab3b1f51bcc60428396
Alex Rypun <arypun@mindk.com>
2020-07-02 20:59:28 +0300

    fix(categories): deny to manipulate with categories for non-admin users

--- api/src/categories/categories.controller.ts
+++ api/src/categories/categories.controller.ts
@@ -1,43 +1,68 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, ParseIntPipe, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
 import { CategoriesService } from './categories.service';
 import { FindCategoriesDto } from './dto/find-categories.dto';
 import { Category } from './category.entity';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { AllowedRoles } from '../common/decorators/allowed-roles.decorator';
+import { RolesGuard } from '../common/guards/roles.guard';
 
 @Controller('categories')
 export class CategoriesController {
-    constructor(private readonly categoriesService: CategoriesService) {}
+  constructor(private readonly categoriesService: CategoriesService) {
+  }
 
-    @Get()
-    @UsePipes(ValidationPipe)
-    getAllCategories(@Query() filters: FindCategoriesDto): Promise<Category[]> {
-        return this.categoriesService.getAllCategories(filters);
-    }
+  @Get()
+  @UsePipes(ValidationPipe)
+  getAllCategories(@Query() filters: FindCategoriesDto): Promise<Category[]> {
+    return this.categoriesService.getAllCategories(filters);
+  }
 
-    @Get('/:id')
-    getCategoryById(@Param('id', ParseIntPipe) id: number): Promise<Category> {
-        return this.categoriesService.getCategoryById(id);
-    }
+  @Get('/:id')
+  getCategoryById(@Param('id', ParseIntPipe) id: number): Promise<Category> {
+    return this.categoriesService.getCategoryById(id);
+  }
 
-    @Post()
-    @UsePipes(ValidationPipe)
-    createCategory(@Body() data: CreateCategoryDto): Promise<Category> {
-        return this.categoriesService.createCategory(data);
-    }
+  @Post()
+  @AllowedRoles('admin')
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @UsePipes(ValidationPipe)
+  createCategory(@Body() data: CreateCategoryDto): Promise<Category> {
+    return this.categoriesService.createCategory(data);
+  }
 
-    @Patch('/:id')
-    @UsePipes(ValidationPipe)
-    updateCategory(
-        @Param('id', ParseIntPipe) id: number,
-        @Body() data: UpdateCategoryDto
-    ): Promise<Category> {
-        return this.categoriesService.updateCategory(id, data);
-    }
+  @Patch('/:id')
+  @AllowedRoles('admin')
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @UsePipes(ValidationPipe)
+  updateCategory(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() data: UpdateCategoryDto
+  ): Promise<Category> {
+    return this.categoriesService.updateCategory(id, data);
+  }
 
-    @Delete('/:id')
-    @HttpCode(204)
-    deleteCategory(@Param('id', ParseIntPipe) id: number): Promise<void> {
-        return this.categoriesService.deleteCategory(id);
-    }
+  @Delete('/:id')
+  @AllowedRoles('admin')
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @HttpCode(204)
+  deleteCategory(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.categoriesService.deleteCategory(id);
+  }
 }
